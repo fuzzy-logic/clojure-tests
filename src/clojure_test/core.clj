b7f66c727e0873ac1fa947c524f0fb2fb75ad4f5
JBH92 <j.bartholomew@outlook.com>
2015-06-09 17:40:33 +0100

    First commit

---
+++ src/clojure_test/core.clj
@@ -0,0 +1,47 @@
+(ns clojure-test.core
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo])
+)
+
+(defn problem-1
+  "Rotate an array of n elements to the right by k steps."
+  [list k]
+  (let [ x (if (> k 0) (- (count list) k) (- k))
+        [head tail] (split-at x list)]
+    (concat tail head))
+)
+
+(defn problem-2
+  "Given two strings w1 and w2, determine if they are isomorphic."
+  [w1 w2]
+  (vec (seq w1))
+)
+
+(defn problem-3
+  "Compare two version numbers v1 and v2. If v1 > v2 return 1, if v1 < v2 return -1, otherwise return 0"
+  [v1 v2]
+  (let [fn-v (fn [v] (map read-string (str/split v #"\D")))
+        version1 (fn-v v1)
+        version2 (fn-v v2)
+        n (max (count version1) (count version2))]
+    (compare (vec(take n (concat version1 (repeat 0)))) (vec(take n (concat version2 (repeat 0))))))
+)
+
+(defn problem-4
+  "Given an input string, reverse the string word by word."
+  [sentence]
+  (str/join " " (reverse (str/split sentence #" ")))
+)
+
+(defn problem-5
+  "Given an input string, reverse the string word by word."
+  [sentence]
+  (str/join " " (reverse (str/split sentence #" ")))
+)
+
+(defn problem-7
+  "Given a collection of numbers, return all possible permutations."
+  [numbers]
+  (combo/permutations numbers)
+)
---
+++ test/clojure_test/core_test.clj
@@ -0,0 +1,9 @@
+(ns clojure-test.core-test
+  (:use midje.sweet)
+  (:use clojure.test)
+  (:require [clojure-test.core :as core]))
+
+(fact "array was not rotated as expected" (core/problem-1 [1 2 3 4 5 6 7 8 9] 2) => [8 9 1 2 3 4 5 6 7])
+(fact "text was incorrectly reversed" (core/problem-4 "This is some test data") => "data test some is This")
+(fact "version numbers were incorrectly ordered" (core/problem-3 "1.3.2" "1.3.2.1") => -1)
+(fact "permutations of vector were incorrectly output" (core/problem-7 [0 1 2]) => 1)
