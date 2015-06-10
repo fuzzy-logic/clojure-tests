749760688d1a1b23987be9a1eb79c744938794dd
JBH92 <j.bartholomew@outlook.com>
2015-06-10 14:58:25 +0100

    fixed merge issue

--- test/clojure_test/core_test.clj
+++ test/clojure_test/core_test.clj
@@ -3,7 +3,6 @@
   (:use clojure.test)
   (:require [clojure-test.core :as core]))
 
-<<<<<<< HEAD
 (fact "array was not rotated as expected" (core/problem-1 [1 2 3 4 5 6 7 8 9] 2)
       => [8 9 1 2 3 4 5 6 7])
 (fact "text was incorrectly reversed" (core/problem-4 "This is some test data")
@@ -14,7 +13,7 @@
       => '([0 1 2] [0 2 1] [1 0 2] [1 2 0] [2 0 1] [2 1 0]))
 (fact "permutations of vector were incorrectly output" (core/problem-8 19)
       => "That right there is a good ol' happy number pardner!!")
-=======
+
 (fact "array was not rotated as expected 1" (core/problem-1 [1 2 3 4 5 6 7 8 9] 2) => [8 9 1 2 3 4 5 6 7])
 (fact "array was not rotated as expected 2" (core/problem-1 [1 2 3 4 5 6 7 8 9] 8) => [2 3 4 5 6 7 8 9 1])
 (fact "array was not rotated as expected 3" (core/problem-1 [1 2 3 4 5 6 7 8 9] 9) => [1 2 3 4 5 6 7 8 9])
@@ -31,4 +30,3 @@
 
 
 (fact "permutations of vector were incorrectly output" (core/problem-7 [0 1 2]) => '([0 1 2] [0 2 1] [1 0 2] [1 2 0] [2 0 1] [2 1 0]))
->>>>>>> c78197860950e50d2313e1423bf19bceddd498bb
