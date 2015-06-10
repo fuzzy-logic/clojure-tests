749d8ed062077b01975d2d2a3fc74973220d6fcd
JBH92 <j.bartholomew@outlook.com>
2015-06-10 14:42:56 +0100

    5 problems completedgit add -A

--- src/clojure_test/core.clj
+++ src/clojure_test/core.clj
@@ -23,13 +23,15 @@
 )
 
 (defn problem-3
-  "Compare two version numbers v1 and v2. If v1 > v2 return 1, if v1 < v2 return -1, otherwise return 0"
+  "Compare two version numbers v1 and v2.
+  If v1 > v2 return 1, if v1 < v2 return -1, otherwise return 0"
   [v1 v2]
   (let [fn-v (fn [v] (map read-string (str/split v #"\D")))
         version1 (fn-v v1)
         version2 (fn-v v2)
         n (max (count version1) (count version2))]
-    (compare (vec(take n (concat version1 (repeat 0)))) (vec(take n (concat version2 (repeat 0))))))
+    (compare (vec (take n (concat version1 (repeat 0))))
+             (vec (take n (concat version2 (repeat 0))))))
 )
 
 (defn problem-4
@@ -53,10 +55,8 @@
 (defn problem-8
   "Given a collection of numbers, return all possible permutations."
   [number]
-  (let [n (map read-string (remove empty? (str/split number #"")))]
-    (cond (= (reduce + (map exp n)) 1) "That right there is a good ol' happy number pardner!!"
-          (= (reduce + (map exp n)) 4) "That isnt a happy number!"
-          :else "yoyoyo"
-    )
-  )
-)
+  (let [n (map read-string (remove empty? (str/split (str number) #"")))
+        y (reduce + (map exp n))]
+    (cond (= y 1) "That right there is a good ol' happy number pardner!!"
+          (= y 4) "That isnt a happy number!"
+          :else (recur y))))
